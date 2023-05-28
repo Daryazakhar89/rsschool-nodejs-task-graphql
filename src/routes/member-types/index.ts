7beb5fb0f555fc37870a7c1ea85b4aad06ffab45
Darya Zakhar <daryazakhar.89@gmail.com>
2023-05-29 03:05:06 +0400

    feat: add logic to member-types endpoints

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -3,12 +3,15 @@ import { idParamSchema } from '../../utils/reusedSchemas';
 import { changeMemberTypeBodySchema } from './schema';
 import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
 
+type ChangeMemberTypeDTO = Partial<Omit<MemberTypeEntity, 'id'>>;
+
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<
-    MemberTypeEntity[]
-  > {});
+  fastify.get('/', async function (request, reply): Promise<MemberTypeEntity[]> {
+    const memberTypes = await fastify.db.memberTypes.findMany();
+    return reply.code(200).send(memberTypes);
+  });
 
   fastify.get(
     '/:id',
@@ -17,8 +20,15 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
-  );
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const { id } = request.params as { id: string };
+      const memberType = await fastify.db.memberTypes.findOne({ key: 'id', equals: id });
+      if (memberType) {
+        return reply.code(200).send(memberType);
+      } else {
+        return reply.code(404).send({ message: 'Member type was not found' });
+      }
+    });
 
   fastify.patch(
     '/:id',
@@ -28,8 +38,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
-  );
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const { id } = request.params as { id: string };
+			const body = request.body as ChangeMemberTypeDTO;
+      const memberType = await fastify.db.memberTypes.findOne({ key: 'id', equals: id });
+      if (memberType) {
+        const changedMemberType = await fastify.db.memberTypes.change(id, body);
+        return reply.code(200).send(changedMemberType);
+      } else {
+        return reply.code(400).send({ message: 'Invalid member type' });
+      }
+  });
 };
 
 export default plugin;
