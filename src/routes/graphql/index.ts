55b8ba84bc81cbd49ec1b624fce44350d07a621e
Darya Zakhar <daryazakhar.89@gmail.com>
2023-05-30 00:39:17 +0400

    feat:  setting graphql endpoint

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,17 +1,31 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import { graphql } from 'graphql';
 import { graphqlBodySchema } from './schema';
+import { Resolver } from './resolvers';
+import { graphQlSchema } from './graphqlSchema';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
+  const resolver = new Resolver(fastify);
+
   fastify.post(
     '/',
     {
       schema: {
-        body: graphqlBodySchema,
+        body: graphqlBodySchema
       },
     },
-    async function (request, reply) {}
+    function (request, reply) {
+      graphql({
+        schema: graphQlSchema,
+        source: request.body.query as string,
+        rootValue: resolver.getResolvers(),
+        variableValues: request.body.variables
+      }).then(response => {
+        reply.send(response);
+      })
+    }
   );
 };
 
